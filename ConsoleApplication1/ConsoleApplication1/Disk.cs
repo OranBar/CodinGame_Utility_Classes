106810cbf7d77b76a4812b0e11fa12cfbe226bee
Oran <baroran@hotmail.com>
2016-12-10 03:34:44 +0100

    Disk now inherits from Vector2. Changes to equals operators

--- ConsoleApplication1/ConsoleApplication1/Disk.cs
+++ ConsoleApplication1/ConsoleApplication1/Disk.cs
@@ -3,15 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 
 
-public class Disk
+public class Disk : Vector2
 {
-    public readonly Vector2 position;
     public readonly Vector2 velocity;
     public readonly double radius;
 
-    public Disk(Vector2 position, Vector2 velocity, double radius)
+    public Disk(Vector2 position, Vector2 velocity, double radius) : base(position.X, position.Y)
     {
-        this.position = position;
         this.velocity = velocity;
         this.radius = radius;
     }
@@ -19,17 +17,27 @@ public class Disk
     public override int GetHashCode() {
         unchecked
         {
-            return 17 * position.GetHashCode() + 23 * velocity.GetHashCode() + 31 * radius.GetHashCode();
+            return 17 * this.GetHashCode() + 23 * velocity.GetHashCode() + 31 * radius.GetHashCode();
         }
     }
 
     public static override bool operator==(Disk d1, Disk d2){
-        return d1.Equals(d2);
+        // If both are null, or both are same instance, return true.
+        if (System.Object.ReferenceEquals(a, b))
+        {
+            return true;
+        }
+
+        // If one is null, but not both, return false.
+        if (((object)a == null) || ((object)b == null))
+        {
+            return false;
+        }
     }
 
     public static override bool operator !=(Disk d1, Disk d2)
     {
-        return d1.Equals(d2) == false;
+        return (d1==d2) == false;
     }
 
     public override bool Equals(object obj)
@@ -43,7 +51,7 @@ public class Disk
         {
             return false;
         }
-        if(position == otherDisk.position
+        if(this == otherDisk
             && velocity == otherDisk.velocity
             && radius == otherDisk.radius)
         {
@@ -59,17 +67,17 @@ public class Disk
      */
     public Disk Move()
     {
-        return new Disk(position + velocity, velocity, radius);
+        return new Disk(this + velocity, velocity, radius);
     }
 
     public Disk Accelerate(Vector2 acceleration)
     {
-        return new Disk(position, velocity + acceleration, radius);
+        return new Disk(this, velocity + acceleration, radius);
     }
 
     public Disk Accelerate(double factor)
     {
-        return new Disk(position, velocity*factor, radius);
+        return new Disk(this, velocity*factor, radius);
     }
 
 
@@ -81,7 +89,7 @@ public class Disk
      */
     public bool WillCollide(Disk other)
     {
-        Vector2 toOther = other.position - position;
+        Vector2 toOther = other - this;
         Vector2 relativeSpeed = velocity - (other.velocity);
         if (relativeSpeed.LengthSquared() <= 0) // No relative movement
         {
